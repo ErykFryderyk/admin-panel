64107eed351819df3852014ecc96885efb7cee72
Eryk Fryderyk <grzeniu098@gmail.com>
2021-11-24 12:41:34 +0100

    add dynamic class to right-sidebar

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,44 @@
 
 const hamburger = document.querySelector('.hamburger');
 const sidebar = document.querySelector('.sidebar');
+const dropdownBtn = document.querySelector('[data-dropdown-btn]');
+const profileBtn = document.querySelector('[data-profile-btn]');
+const dropdown = document.querySelector('[data-dropdown]');
+const profileDropdown = document.querySelector('[data-profile-dropdown]');
+const dotsBtn = document.querySelector('[data-settings-btn]');
+const sidebarCloseBtn = document.querySelector('[data-settings-close-btn]');
+const settingsSidebar = document.querySelector('[data-right-sidebar]');
 
 
 const toggleFunction = () => {
+    
     sidebar.classList.toggle('toggle');
+}
+
+const showDropdown = (el) => {
+
+    if (el.classList.contains('show')){
+        el.classList.add('hide')
+        el.classList.remove('show')
+    }else{
+        el.classList.add('show')
+        el.classList.remove('hide')
+    }
 }
 
 
 hamburger.addEventListener('click', toggleFunction);
+dropdownBtn.addEventListener('click', function() {
+    const elName = dropdown;
+    showDropdown(elName);
+});
+profileBtn.addEventListener('click', function() {
+    const elName = profileDropdown;
+    showDropdown(elName);
+});
+dotsBtn.addEventListener('click', function() {
+    settingsSidebar.classList.toggle('right-sidebar--active');
+})
+sidebarCloseBtn.addEventListener('click', function() {
+    settingsSidebar.classList.toggle('right-sidebar--active');
+})
